efbe2958b9bdedd502487673190227fc1b7708b6
radcli14 <radcli14@gmail.com>
2022-10-05 22:29:51 -0400

    Spawns new marbles

---
+++ GravityWell/Assets/MarbleBehavior.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0f, 0f, 0.9f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- GravityWell/Assets/Spawner.cs
+++ GravityWell/Assets/Spawner.cs
@@ -4,22 +4,47 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public GameObject marble;
+    List<MarbleBehavior> marbles;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var foundMarbles = FindObjectsOfType<MarbleBehavior>();
+        marbles = new List<MarbleBehavior>(foundMarbles);
     }
 
     // Update is called once per frame
-    float elapsed = 0f;
     void Update()
     {
+
+    }
+
+    float elapsed = 5f;
+    private void FixedUpdate()
+    {
         elapsed += Time.deltaTime;
-        if (elapsed >= 1f)
+        if (elapsed >= 5f)
         {
             // Spawn a new marble
-            print("Spawn!");
+            var newMarble = Instantiate(marble);
+            var behavior = newMarble.gameObject.GetComponent<MarbleBehavior>();
+            marbles.Add(behavior);
+            //marbles.Add(newMarble);
+
+            // Reset the clock
             elapsed = 0f;
         }
+
+        // Check if marbles have fallen, if they have, DESTROY!
+        marbles.ForEach(marble =>
+            {
+                if (marble.transform.position.y < 0)
+                {
+                    Destroy(marble);
+                    marbles.Remove(marble);
+                }
+            }
+        );
     }
 }
