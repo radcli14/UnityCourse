08e7de9e50c2368e63631d77d9e0f30aec588db2
radcli14 <radcli14@gmail.com>
2022-10-24 22:20:44 -0400

    added left and right turn animations

--- DC/Assets/Fly.cs
+++ DC/Assets/Fly.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Fly : MonoBehaviour
 {
+Transform wrightTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        wrightTransform = transform.GetChild(0);
     }
 
     // Update is called once per frame
@@ -19,5 +21,7 @@ public class Fly : MonoBehaviour
     void FixedUpdate()
     {
         transform.Translate(new Vector3(0, 0, 0.1f));
+        float roll = wrightTransform.eulerAngles.z;
+        transform.Rotate(new Vector3(0f, -3.14f * Mathf.Sin(0.01745329251f * roll), 0f));
     }
 }
