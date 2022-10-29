23f8274fed116e77a3f944ebcb8df3b67faf26d0
radcli14 <radcli14@gmail.com>
2022-10-29 10:39:02 -0400

    a fair amount of reorganization, and a wright flyer that flies itself, with nice lighting on the capitol and white house

--- DC/Assets/Fly.cs
+++ DC/Assets/Fly.cs
@@ -10,43 +10,20 @@ public class Fly : MonoBehaviour
     // Scales the yaw rate as a function of the roll angle
     float yawScale = -3.14f;
 
-    // Setting for how far the plane has rolled, 0 is hard left, 1 hard right
-    float rollNormTime = 0f;
-    float rollBlend = 0.25f;
-    float rollTarget = 0.5f;
-
-    Animator animator;
-
     // Start is called before the first frame update
     void Start()
     {
         wrightTransform = transform.GetChild(0);
-
-        animator = GetComponent<Animator>();
-        animator.SetFloat("Blend", rollBlend);
     }
 
     // Update is called once per frame
-    float dt = 0f;
-    float randomizeAtTime = 1.333f;
     void Update()
     {
-        // If sufficient time has passed, update the intended roll angle
-        dt += Time.deltaTime;
-        if (dt >= randomizeAtTime) 
-        {
-            dt = 0f;
-            rollTarget = Random.Range(0f, 1f);
-        }
+
     }
 
     void FixedUpdate()
     {
-        // Update the blended roll angle in the animator
-        rollBlend += 0.1f * (rollTarget - rollBlend);
-        rollBlend = Mathf.Clamp(rollBlend, 0f, 1f);
-        animator.SetFloat("Blend", rollBlend);
-
         // Update the yaw angle as a function of the roll
         float roll = wrightTransform.eulerAngles.z;
         float rollSine = Mathf.Sin(0.01745329251f * roll);
